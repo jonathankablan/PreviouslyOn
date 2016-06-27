6b810496b1f22233d2cc8984b782bd7a747aab73
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-27 17:26:08 +0200

    Début de methode de login dans le WelcomeCtrl, avec un tit popup d'error en cas d'erreur à la connexion. Reste a traiter le success

--- PreviouslyOn/www/js/controllers.js
+++ PreviouslyOn/www/js/controllers.js
@@ -7,5 +7,21 @@
     var controllers = angular.module("previously_on.controllers", []);
 
     controllers.controller("WelcomeCtrl", function () {
+    controllers.controller("WelcomeCtrl", function (UserService, $ionicPopup) {
+        this.user = {};
+
+        var self = this;
+        this.login = function (user) {
+            UserService.login(user, function (res) {
+                alert(res.data);
+            }, function (err) {
+                $ionicPopup.alert({
+                    title: "Uh-oh... something went wrong !",
+                    template: err.data.errors[0].text
+                }).then(function () {
+                    self.user.password = "";
+                });
+            });
+        };
     });
 }());
