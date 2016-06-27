e4ce7d86e81005c1efb7aa6d9a6aef24d538905f
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-27 17:42:02 +0200

    A la connexion, stockage des credentials recupérés dans le service puis dans le localStorage, avec le rememberMe, reste à faire la redirection

--- PreviouslyOn/www/js/controllers.js
+++ PreviouslyOn/www/js/controllers.js
@@ -11,8 +11,14 @@
 
         var self = this;
         this.login = function (user) {
-            UserService.login(user, function (res) {
-                alert(res.data);
+            UserService.login(user, function (response) {
+                var credentials = {};
+                credentials.id = response.data.user.id;
+                credentials.login = response.data.user.login;
+                credentials.token = response.data.token;
+                credentials.rememberMe = user.rememberMe || false;
+                UserService.setCredentials(credentials);
+                self.user = {};
             }, function (err) {
                 $ionicPopup.alert({
                     title: "Uh-oh... something went wrong !",
