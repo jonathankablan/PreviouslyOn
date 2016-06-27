12a16de690dfa19d69d323832a2747dfd7cfc109
laidet_r <rodolphe.laidet@epitech.eu>
2016-06-27 17:23:18 +0200

    Création d'un UserServie, qui comporte l'api-key, l'api-url et les params nécessaires pour faire des requetes (version + key)

--- PreviouslyOn/www/js/services.js
+++ PreviouslyOn/www/js/services.js
@@ -7,4 +7,16 @@
     var services = angular.module("previously_on.services", [
         "angular-md5"
     ]);
+
+    services.service("UserService", function ($http, md5) {
+        this.apiKey = "6b01f237a3af";
+        this.apiUrl = "https://api.betaseries.com/";
+        this.params = {
+            params: {
+                v: 2.4,
+                key: this.apiKey
+            }
+        };
+
+    });
 }());
